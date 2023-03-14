6e95144af8a5573be6be3b286018055b5312271b
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-03-14 13:22:16 +0300

    feat: add js .map aboutUs content

---
+++ data/aboutUsContent.js
@@ -0,0 +1,40 @@
+const aboutUsContent = [
+    {
+        "src": "../img/grid.jpg",
+        "title": "Kayaking. Through the Ushach lakes.",
+        "text": "June 30 - July 03, 2017"
+    },
+    {
+        "src": "../img/grid.jpg",
+        "title": "Kayaking. Perekul - Sula",
+        "text": "07 - 09 May 2017"
+    },
+    {
+        "src": "../img/logo.png",
+        "title": "Weekend bike trip to Naliboki",
+        "text": "April 22-24, 2017"
+    },
+    {
+        "src": "../img/logo.png",
+        "title": "Kayaking. Gavya - Neman",
+        "text": "June 02-04, 2017"
+    },
+    {
+        "src": "../img/logo.png",
+        "title": "Kayaking. Molchad river",
+        "text": "June 16 - 18, 2017"
+    }
+    ,
+    {
+        "src": "../img/logo.png",
+        "title": "Kayaking. River Ptich. From Samokhvalov to Rudensk",
+        "text": "04 - 06 August 2017"
+    },
+    {
+        "src": "../img/grid.jpg",
+        "title": "Kayaking. Rivers Ptich - Titovka - Svisloch - Talka",
+        "text": "August 25-27, 2017 Start near Rudensk, finish - Talka station. Titovka completed completely. Talc - a couple of kilometers against the current. Interesting route. 1 update. Experienced group will pass over the weekend, beginners - 3-4 days."
+    }
+]
+
+export default aboutUsContent
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 import videoContent from '../data/videoContent.js'
+import aboutUsContent from '../data/aboutUsContent.js'
 
 const videoContentWrapper = document.getElementById('videoContentWrapper')
+const aboutUsContentWrapper = document.getElementById('aboutUsContentWrapper')
+
 
 const videoContentHtml = videoContent.map(video =>
     `<div class="col-lg-6 col-md-12 col-xs-12">
@@ -21,4 +24,16 @@ const videoContentHtml = videoContent.map(video =>
 </div>`
 ).join('')
 
+const aboutUsContentHTML = aboutUsContent.map(card =>
+    `
+    <div class="col-xs-12 col-sm-4 col-lg-3" data-toggle="modal" data-target="#aboutUsmodal"
+        style="cursor: pointer;">
+        <img src="${card.src}" alt="${card.title}" class="w-100">
+        <h4>${card.title}</h4>
+        <p>${card.text}</p>
+    </div>`
+).join('')
+
+
 videoContentWrapper.innerHTML = videoContentHtml
+aboutUsContentWrapper.innerHTML = aboutUsContentHTML 
